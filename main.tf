572f958792fc4416bfb8c36ab1ab021235bb4058
Dennis Riemenschneider <dennis.riemenschneider@kiongroup.com>
2021-01-13 10:49:51 +0100

    add module terraform files main.tf and variables.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+# external data provider to get the AzureRM Client Secret, which isn't available as any terraform variable
+data "external" "client_secret" {
+  program = ["/bin/sh", "-c", "${abspath(path.module)}/scripts/clientsecret.sh"]
+}
+
+# the null resource which runs the local-exec provisioner triggered by a change of the kubernetes_node_version
+resource "null_resource" "node_pool_version" {
+  triggers = {
+    kubernetes_node_version = var.kubernetes_node_version
+  }
+
+  provisioner "local-exec" {
+    command = "${abspath(path.module)}/scripts/update_node_version.sh"
+    environment = {
+      KUBERNETES_NODE_VERSION       = var.kubernetes_node_version
+      ARM_CLIENT_SECRET_URL_ENCODED = urlencode(data.external.client_secret.result.clientsecret)
+      RESOURCE_GROUP_NAME           = var.resource_group_name
+      CLUSTER_NAME                  = var.kubernetes_cluster_name
+    }
+    interpreter = ["/bin/sh", "-c"]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "kubernetes_node_version" {
+  type        = string
+  default     = "empty"
+  description = "The Kubernetes Version for the default node pool."
+}
+
+variable "resource_group_name" {
+    type = string
+    description = "The name of the Resource Group which contains the Kubernetes Cluster."
+}
+
+variable "kubernetes_cluster_name" {
+    type = string
+    description = "The name of the Kubernetes Cluster."
+}
